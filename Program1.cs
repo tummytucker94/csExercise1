91a1c81b9832ccad9eb9515a917baad01a49bc7c
tummytucker94 <52971811+tummytucker94@users.noreply.github.com>
2020-08-11 16:04:58 -0400

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSExcercise1
+{
+	class CSExercise1
+	{
+		static void Main(string[] args)
+		{
+		    string message = "Hello";
+		    string myName = Console.ReadLine();
+		    string result = message + ": " + myName;
+		    Console.WriteLine(result);
+		}
+	}
+}
